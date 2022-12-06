5bdc11fc34efa9d9467f9c8baafccbea3488d366
Hyun <hickee32@gmail.com>
2022-12-06 22:36:02 +0900

    median
    세 수 중 중간 값

---
+++ median.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace median {
+    internal class median {
+        static void Main(string[] args) {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"중간값은 {med(a, b, c)}");
+        }
+
+        static int med(int a,int b,int c) {
+            if (a>=b) {
+                if (b>=c) {
+                    return b;
+                }
+                else if (a<=c) {
+                    return a;
+                }
+                else {
+                    return c;
+                }
+            }
+            else if (a>c) {
+                return a;
+            }
+            else if (b>c) {
+                return c;
+            }
+            else {
+                return b;
+            } 
+         }
+    }
+}
