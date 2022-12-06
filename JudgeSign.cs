e9e82519b6a7f5c2cc19870ae3436a2f2e5f1895
Hyun <hickee32@gmail.com>
2022-12-07 00:28:19 +0900

    JudgeSign

---
+++ JudgeSign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JudgeSign {
+    internal class JudgeSign {
+        static void Main(string[] args) {
+            Console.WriteLine("정수를 입력하세요");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n>0) {
+                Console.WriteLine("양수");
+            }
+            else if (n<0) {
+                Console.WriteLine("음수");
+            }
+            else {
+                Console.WriteLine("0입니다.");
+            }
+
+        }
+    }
+}
