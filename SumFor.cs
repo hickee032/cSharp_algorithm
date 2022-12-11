039d738a4ec76794d1568af2f220ef16482479f7
Hyun <hickee32@gmail.com>
2022-12-11 19:29:17 +0900

    For

---
+++ SumFor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForSum {
+    internal class SumFor {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sum+= i;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ SumFor02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForSum2 {
+    internal class SumFor02 {
+        static void Main(string[] args) {
+            //n 을 양수만 입력
+
+            int n;
+
+            do {
+                n = int.Parse(Console.ReadLine());
+            } while (n<=0);
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                if (i<n) {
+                    Console.Write(i + " + ");
+                }
+                else {
+                    Console.Write(i + " = ");
+                }
+
+                sum += i;
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
