3cc10304597d9a280e32c4c8fb464d87fe62897c
Hyun <hickee32@gmail.com>
2022-12-11 19:29:41 +0900

    While

---
+++ SumWhile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileSum {
+    internal class SumWhile {
+        static void Main(string[] args) {
+            //while 1 ~ n
+
+            int n = int.Parse(Console.ReadLine());
+
+            int i = 1;
+            int sum = 0;
+            while (i <= n) {
+            
+                sum+= i;
+                i++;
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
